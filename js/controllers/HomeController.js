43da438be6e50b2724520052853b52abfe6dd147
drinkingTea <teatocode@gmail.com>
2016-01-07 19:11:24 -0500

    empty

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,7 +1,4 @@
 app.controller("HomeController", ["$scope", "homeData", function($scope, homeData) {
 	homeData.success(function(data) {
-		$scope.data = data;
-		$scope.leftCol = [data.skills, data.cycling, data.itThings];
-		$scope.rightCol = [data.devPort, data.about]
 	});
 }])
